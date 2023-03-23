50f7e6d036acb00f3d6c2e62015eea7d5ab2b4ca
Taís Sousa <nascimentotaais@gmail.com>
2023-03-23 16:52:52 -0300

    segundo commit

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import Flight, Airport
+
+# Register your models here.
+admin.site.register(Flight)
+admin.site.register(Airport)
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Flight, Airport
 
 # Create your views here.
-def index (request):
-    return HttpResponse("Hello, word")
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
